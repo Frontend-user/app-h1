e743e59dbbb9fe980d89f23c84d5f837fcd56212
Robert <robersargsyan2023@gmail.com>
2024-01-31 17:54:24 +0300

    versel deploy

--- dist/index.js
+++ dist/index.js
@@ -44,9 +44,26 @@ app.delete('/testing/all-data', (req, res) => {
 app.get('/videos', (req, res) => {
     res.send(db.videos);
 });
+app.get('/videos/:id', (req, res) => {
+    let findVideo = db.videos.find(v => v.id === +req.params.id);
+    if (findVideo) {
+        res.status(200).send(findVideo);
+    }
+    else {
+        res.sendStatus(404);
+    }
+});
+app.delete('/videos/:id', (req, res) => {
+    let findVideoIndex = db.videos.findIndex(v => v.id === +req.params.id);
+    if (findVideoIndex >= 0) {
+        db.videos.splice(findVideoIndex, 1);
+        res.sendStatus(204);
+    }
+    else {
+        res.sendStatus(404);
+    }
+});
 app.post('/videos', (req, res) => {
-    console.log(req.body.title);
-    let isValid = true;
     const newVideo = {
         id: Date.now(),
         'title': req.body.title,
@@ -54,21 +71,14 @@ app.post('/videos', (req, res) => {
         "canBeDownloaded": false,
         "minAgeRestriction": null,
         "createdAt": "2024-01-29T09:17:06.754Z",
-        // "createdAt": new Date().toISOString(),
         "publicationDate": "2024-01-30T09:17:06.754Z",
         "availableResolutions": [
             "P144"
         ]
     };
-    if (typeof (req.body.title) !== 'string' || typeof (req.body.author) !== 'string') {
-        isValid = false;
-    }
     let myErrorsMessages = [];
     checkValid('title', req.body.title, 'string', 40);
     checkValid('author', req.body.author, 'string', 20);
-    // checkValid('canBeDownloaded', req.body.canBeDownloaded, 'boolean', 100)
-    // checkValid('minAgeRestriction', req.body.minAgeRestriction, 'number', 18)
-    // checkValid('publicationDate', req.body.publicationDate, 'string', 180)
     let arr = req.body.availableResolutions;
     for (let i of arr) {
         if (i[0] !== 'P') {
@@ -94,8 +104,6 @@ app.post('/videos', (req, res) => {
             }
         }
         else if (typeof (data) !== type || maxLength < data.length && type !== 'boolean') {
-            console.log(data.length, 'data length');
-            console.log(data, 'data');
             myErrorsMessages.push({
                 message: 'error',
                 field: name
@@ -111,39 +119,6 @@ app.post('/videos', (req, res) => {
             errorsMessages: myErrorsMessages
         });
     }
-});
-app.get('/videos/:id', (req, res) => {
-    const videoId = Number(req.params.id);
-    console.log(videoId, 'videoid');
-    const video = db.videos.find(v => v.id === videoId);
-    if (video) {
-        res.send(video);
-    }
-    res.send(404);
-});
-app.delete('/videos/:id', (req, res) => {
-    // for(let i = 0; i < db.videos.length;i++){
-    //     if(db.videos[i].id===+req.params.id){
-    //         db.videos.splice(i,1)
-    //         res.send(204)
-    //         return
-    //     }
-    // }
-    // res.send(404)
-    const videoId = Number(req.params.id);
-    console.log(videoId, 'videoid');
-    const videoToDelete = db.videos.find(v => v.id === videoId);
-    if (videoToDelete) {
-        for (let video in db.videos) {
-            console.log(db.videos[+video].id, 'veddd');
-            if (db.videos[+video].id === videoToDelete.id) {
-                db.videos.splice(Number(video), 1);
-                res.send(204);
-                return;
-            }
-        }
-    }
-    res.send(404);
 });
 app.put('/videos/:id', (req, res) => {
     let isValid = true;
@@ -172,26 +147,20 @@ app.put('/videos/:id', (req, res) => {
             }
         }
         else if (typeof (data) !== type || maxLength < data.length && type !== 'boolean') {
-            console.log(data.length, 'data length');
-            console.log(data, 'data');
             myErrorsMessages.push({
                 message: 'error',
                 field: name
             });
         }
     }
-    console.log(myErrorsMessages, 'errrors');
     let findVideo = db.videos.find(v => v.id === Number(req.params.id));
     if (!findVideo) {
         res.sendStatus(404);
-        console.log(1);
     }
     if (myErrorsMessages.length > 0) {
         res.status(400).send({ errorsMessages: myErrorsMessages });
-        console.log(2);
     }
     else if (findVideo && isValid !== false) {
-        console.log(3);
         findVideo.title = req.body.title;
         findVideo.author = req.body.author;
         findVideo.availableResolutions = req.body.availableResolutions;
--- src/index.ts
+++ src/index.ts
@@ -56,9 +56,28 @@ app.get('/videos', (req: Request, res: Response) => {
 })
 
 
+
+
+app.get('/videos/:id', (req: Request, res: Response) => {
+    let findVideo = db.videos.find(v => v.id === +req.params.id)
+    if (findVideo) {
+        res.status(200).send(findVideo)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    let findVideoIndex = db.videos.findIndex(v => v.id === +req.params.id)
+    if (findVideoIndex >= 0) {
+        db.videos.splice(findVideoIndex, 1)
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
+})
 app.post('/videos', (req: Request, res: Response) => {
-    console.log(req.body.title)
-    let isValid = true
     const newVideo: TVideo = {
         id: Date.now(),
         'title': req.body.title,
@@ -66,21 +85,14 @@ app.post('/videos', (req: Request, res: Response) => {
         "canBeDownloaded": false,
         "minAgeRestriction": null,
         "createdAt": "2024-01-29T09:17:06.754Z",
-        // "createdAt": new Date().toISOString(),
         "publicationDate": "2024-01-30T09:17:06.754Z",
         "availableResolutions": [
             "P144"
         ]
     }
-    if (typeof (req.body.title) !== 'string' || typeof (req.body.author) !== 'string') {
-        isValid = false
-    }
     let myErrorsMessages:Object[] = []
     checkValid('title', req.body.title, 'string', 40)
     checkValid('author', req.body.author, 'string', 20)
-    // checkValid('canBeDownloaded', req.body.canBeDownloaded, 'boolean', 100)
-    // checkValid('minAgeRestriction', req.body.minAgeRestriction, 'number', 18)
-    // checkValid('publicationDate', req.body.publicationDate, 'string', 180)
     let arr = req.body.availableResolutions
     for(let i of arr){
         if( i[0]!== 'P'){
@@ -105,8 +117,6 @@ app.post('/videos', (req: Request, res: Response) => {
                 })
             }
         } else if (typeof (data) !== type || maxLength < data.length && type !== 'boolean') {
-            console.log(data.length, 'data length')
-            console.log(data, 'data')
             myErrorsMessages.push({
                 message: 'error',
                 field: name
@@ -125,42 +135,6 @@ app.post('/videos', (req: Request, res: Response) => {
 
 
 })
-
-app.get('/videos/:id', (req: Request, res: Response) => {
-    const videoId = Number(req.params.id)
-    console.log(videoId, 'videoid')
-    const video = db.videos.find(v => v.id === videoId)
-    if (video) {
-        res.send(video)
-    }
-    res.send(404)
-})
-app.delete('/videos/:id', (req: Request, res: Response) => {
-    // for(let i = 0; i < db.videos.length;i++){
-    //     if(db.videos[i].id===+req.params.id){
-    //         db.videos.splice(i,1)
-    //         res.send(204)
-    //         return
-    //     }
-    // }
-    // res.send(404)
-    const videoId = Number(req.params.id)
-    console.log(videoId, 'videoid')
-    const videoToDelete = db.videos.find(v => v.id === videoId)
-    if (videoToDelete) {
-
-        for (let video in db.videos) {
-            console.log(db.videos[+video].id, 'veddd')
-            if (db.videos[+video].id === videoToDelete.id) {
-                db.videos.splice(Number(video), 1)
-                res.send(204)
-                return
-            }
-        }
-    }
-    res.send(404)
-})
-
 app.put('/videos/:id', (req: Request, res: Response) => {
     let isValid = true
     if (typeof (req.body.title) !== 'string' || typeof (req.body.author) !== 'string') {
@@ -187,8 +161,6 @@ app.put('/videos/:id', (req: Request, res: Response) => {
                 })
             }
         } else if (typeof (data) !== type || maxLength < data.length && type !== 'boolean') {
-            console.log(data.length, 'data length')
-            console.log(data, 'data')
             myErrorsMessages.push({
                 message: 'error',
                 field: name
@@ -196,20 +168,14 @@ app.put('/videos/:id', (req: Request, res: Response) => {
         }
     }
 
-    console.log(myErrorsMessages, 'errrors')
     let findVideo = db.videos.find(v => v.id === Number(req.params.id))
     if (!findVideo) {
         res.sendStatus(404)
-        console.log(1)
-
     }
 
     if (myErrorsMessages.length > 0) {
         res.status(400).send({errorsMessages: myErrorsMessages})
-        console.log(2)
     } else if (findVideo && isValid !== false) {
-        console.log(3)
-
         findVideo.title = req.body.title
         findVideo.author = req.body.author
         findVideo.availableResolutions = req.body.availableResolutions
